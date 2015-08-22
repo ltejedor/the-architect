2a55cd9384828e5273321aa15fad9abed5e67307
Leandra Tejedor <lmtejedor@gmail.com>
2015-08-21 19:26:04 -0700

    first commit

---
+++ sketch.js
@@ -0,0 +1,20 @@
+/*
+  Empty example
+ */
+
+
+
+function setup() {
+	var cnv = createCanvas(windowWidth, windowHeight);
+
+	textSize(32);
+	textAlign(CENTER);
+	text("what is the name of our game even?", width/2, height/2);
+	text("be da boss", width/2, height/2+20);
+}
+
+function draw() {
+
+}
+
+
