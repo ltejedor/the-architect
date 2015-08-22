4e04dcd164305c6600fafdc52fd0f58916dddcbd
Leandra Tejedor <lmtejedor@gmail.com>
2015-08-21 21:10:14 -0700

    added a button class

--- sketch.js
+++ sketch.js
@@ -7,14 +7,43 @@
 function setup() {
 	var cnv = createCanvas(windowWidth, windowHeight);
 
-	textSize(32);
+	textSize(52);
 	textAlign(CENTER);
-	text("what is the name of our game even?", width/2, height/2);
-	text("be da boss", width/2, height/2+20);
+	background(5, 5, 10);
 }
 
 function draw() {
+	startScreen();
+}
+
+function startScreen(){
+	var r = Math.round(Math.random() * 255);
+	var g = Math.round(Math.random() * 255);
+	var b = Math.round(Math.random() * 255);
+
+
+	fill(r, g , b);
+
+	text("THE ARCHITECT", width/2, height/2);
+
+
+	var startBtn = new NewButton(width/2, height/2 + 50, 'START');
+	startBtn.showNewBtn();
 
 }
 
+function NewButton(posX, posY, text){
+	this.posX = posX;
+	this.posY = posY;
+	this.butText = text;
+}
 
+NewButton.prototype.showNewBtn = function(){
+	rectMode(CENTER);
+	fill(50, 50, 150);
+	rect(this.posX, this.posY, 100, 40);
+
+	fill(255);
+	textSize(12);
+	text(this.butText, this.posX, this.posY + 4);
+}
